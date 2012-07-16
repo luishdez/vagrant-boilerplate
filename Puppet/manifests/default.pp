6ca2c046806c491009cabee8bad99ddb1435b9ec
Luis Munoz <luis.munoz.hdez@gmail.com>
2012-07-16 03:18:51 +0200

    Add Puppetfile, firewall module and open port 80 by default

--- Puppet/manifests/default.pp
+++ Puppet/manifests/default.pp
@@ -4,7 +4,23 @@ node default {
   include basic
   include php
   include httpd
+  include firewall
 
-  httpd::vhost::add { 'test.vhost': }
+  firewall { "00080 http on port 80":
+    proto => "tcp",
+    dport => "80",
+    action => "accept",
+    notify => Exec["iptables-save"]
+  }
 
+  exec { "iptables-save":
+    command => $operatingsystem ? {
+      "debian" => "/sbin/iptables-save > /etc/iptables/rules.v4",
+      /(RedHat|CentOS)/ => "/sbin/iptables-save > /etc/sysconfig/iptables",
+    },
+    refreshonly => true,
+    notify => Service["iptables"],
+  }
+
+  httpd::vhost::add { 'localhost.vhost': }
 }
