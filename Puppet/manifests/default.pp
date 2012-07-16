e18387ab7813cbf2bd0de7b2a6cea00fbd4b3aba
Luis Munoz <luis.munoz.hdez@gmail.com>
2012-07-16 22:14:57 +0200

    Update httpd setup to follow new method of the module

--- Puppet/manifests/default.pp
+++ Puppet/manifests/default.pp
@@ -1,13 +1,19 @@
 # manifest/default.pp
 node default {
+
   include repositories
   include basic
   include php
-  include httpd
 
   service { 'iptables':
     ensure => 'stopped',
     enable => false,
+  }
+
+  httpd::setup { 'httpdInstall':
+    httpdSetup => {
+      'Listen' => '*:80'
+    }
   }
 
   httpd::vhost::add { 'localhost.dev':
