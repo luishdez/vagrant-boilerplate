e830c3460a465e04bb6b943d4758a51fc8cb1e1d
Luis Munoz <luis.munoz.hdez@gmail.com>
2012-07-20 05:57:05 +0200

    Change the way to install , now uses command instead Package from puppet

--- Puppet/manifests/basic.pp
+++ Puppet/manifests/basic.pp
@@ -25,9 +25,8 @@ class basic {
     subscribe => Package['ntp'],
   }
 
-  package { "update-pcre":
-    provider => rpm,
-    ensure   => latest,
-    source   => "ftp://ftp.pbone.net/mirror/ftp.sourceforge.net/pub/sourceforge/l/li/linuxrpms/centos5/pcre/pcre-8.12-1.x86_64.rpm",
+  exec { "update-pcre":
+    command => "rpm -Uvh ftp://ftp.pbone.net/mirror/ftp.sourceforge.net/pub/sourceforge/l/li/linuxrpms/centos5/pcre/pcre-8.12-1.x86_64.rpm",
+    path    => "/usr/local/bin/:/bin/",
   }
 }
